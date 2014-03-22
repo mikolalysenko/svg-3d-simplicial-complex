6df32a8e7ab2b4a094f5ad5123dacf690c90aff9
Mikola Lysenko <mikolalysenko@gmail.com>
2014-03-22 18:42:41 -0500

    added color rendering

--- render.js
+++ render.js
@@ -4,6 +4,7 @@ module.exports = renderComplex
 
 var splitPolygon = require("split-polygon")
 var glmatrix = require("gl-matrix")
+var normals = require("normals")
 var vec4 = glmatrix.vec4
 var mat4 = glmatrix.mat4
 
@@ -12,7 +13,7 @@ var identity = [1,0,0,0,
                 0,0,1,0,
                 0,0,0,1]
 
-var frustum = [
+var frustum = [ 
   [ 1, 0, 0,-1],
   [-1, 0, 0,-1],
   [ 0, 1, 0,-1],
@@ -34,31 +35,30 @@ function transformVertices(vertices, matrix) {
 }
 
 function zSortCells(vertices, cells) {
-  var taggedCells = cells.map(function(c) {
-    var clipProject = frustum.reduce(function(poly, plane) {
+  var taggedCells = []
+  for(var i=0; i<cells.length; ++i) {
+    var c = cells[i]
+    var poly = frustum.reduce(function(poly, plane) {
       if(poly.length > 0) {
         return splitPolygon.negative(poly, plane)  
       }
       return poly
     }, c.map(function(i) {
-        return vertices[i]
-      }
-    )).map(function(v) {
+      return vertices[i]
+    })).map(function(v) {
       return [v[0]/v[3], v[1]/v[3], v[2]/v[3]]
     })
-    var zmax = clipProject.reduce(function(vertex, z) {
+    var zmax = poly.reduce(function(z, vertex) {
       return Math.max(z, vertex[2])
     }, -Infinity)
-    return [zmax, clipProject]
-  }).filter(function(pair) {
-    return (pair[0] > -Infinity) || isNaN(pair[0])
-  })
+    if(zmax > 0) {
+      taggedCells.push([zmax, poly, i])
+    }
+  }
   taggedCells.sort(function(a, b) { 
-    return a[0] - b[0]
+    return b[0] - a[0]
   })
-  return taggedCells.map(function(x) {
-    return x[1]
-  })
+  return taggedCells
 }
 
 function renderComplex(vertices, cells, options) {
@@ -67,6 +67,9 @@ function renderComplex(vertices, cells, options) {
   var view = options.view || identity
   var projection = options.projection || identity
   var viewport = options.viewport || [[-1,-1], [1,1]]
+
+  //Compute face normals
+  var norms = normals.faceNormals(cells, vertices)
 
   //Construct model-view-projection matrix
   var mvp = new Array(16)
@@ -79,12 +82,19 @@ function renderComplex(vertices, cells, options) {
   //Sort polygons by z distance
   var zcells = zSortCells(clipVerts, cells)
 
+  
   //Splat cells to list
-  return zcells.map(function(cell) {
-    return '<polygon points="' + cell.map(function(v) { 
-      var x = 0.5 * (v[0] + 1.0) * (viewport[1][0] - viewport[0][0]) + viewport[0][0]
-      var y = 0.5 * (1.0 - v[1]) * (viewport[1][1] - viewport[0][1]) + viewport[0][1]
-      return x + "," + y
+  return zcells.map(function(pair) {
+    var cell = pair[1]
+    var index = pair[2]
+    var n = norms[index]
+    var fillStr = n.map(function(c) {
+      return Math.min(Math.max(128*(1+c), 0), 255)|0
+    }).join()
+    return '<polygon fill="rgb(' + fillStr + ')" points="' + cell.map(function(v) { 
+        var x = 0.5 * (v[0] + 1.0) * (viewport[1][0] - viewport[0][0]) + viewport[0][0]
+        var y = 0.5 * (1.0 - v[1]) * (viewport[1][1] - viewport[0][1]) + viewport[0][1]
+        return x + "," + y
     }).join(" ") + '"/>'
   }).join("\n")
 }
--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
-"use strict"
+  "use strict"
 
 var bunny  = require("bunny")
 var mat4   = require("gl-matrix").mat4
 var render = require("../render.js")
 
-console.log('<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256" viewport="0 0 256 256" version="1.1">')
+console.log('<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewport="0 0 256 256" version="1.1">')
 console.log(render(bunny.positions, bunny.cells, {
   view: mat4.lookAt(mat4.create(), [5, 10, 20], [0,2,0], [0,1,0]),
   projection: mat4.perspective(mat4.create(),
@@ -12,6 +12,6 @@ console.log(render(bunny.positions, bunny.cells, {
     1.0,
     0.1,
     1000.0),
-  viewport: [[0,0], [256,256]]
+  viewport: [[0,0], [512,512]]
 }))
 console.log("</svg>")
